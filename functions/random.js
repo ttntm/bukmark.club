6f2c92b0c0f69f6b27f184bd84def89081a77b7d
ttntm <ttntm@pm.me>
2024-03-01 17:43:40 +0100

    fix: random fn

--- functions/random.js
+++ functions/random.js
@@ -2,12 +2,7 @@ const path = require('path')
 const pages = require('./pages.json')
 
 exports.handler = async(event, context) => {
-  const current = path.basename(event.headers.referer)
-  let random
-
-  do {
-    random = pages[Math.floor(Math.random() * pages.length)]
-  } while (random === current)
+  const random = pages[Math.floor(Math.random() * pages.length)]
 
   return {
     statusCode: 302,
